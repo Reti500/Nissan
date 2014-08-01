2328e812f71219cf3ae5beabc0692e5614969f17
solorulo <solorulo@hotmail.com>
2014-08-01 05:56:55 -0500

    Se puso la accion para cerrar video de cliche y pasar a 360

--- xtrail_brand/js/index.js
+++ xtrail_brand/js/index.js
@@ -110,6 +110,29 @@ $(document).ready(function() {
 	$("#lpriv").magnificPopup({
 		type: 'ajax'
 	});
+	$('.option').magnificPopup({
+		callbacks: {
+		    open: function() {
+		      
+		      
+		     // this part overrides "close" method in MagnificPopup object
+		      $.magnificPopup.instance.close = function () {
+		      
+		          $('#destruir').fadeOut(500);
+					$('#cliches').fadeOut(500);
+					$('#el360').fadeIn(500);
+		      
+		           // "proto" variable holds MagnificPopup class prototype
+		           // The above change that we did to instance is not applied to the prototype, 
+		           // which allows us to call parent method:
+		           $.magnificPopup.proto.close.call(this);
+		      }; 
+		      // you may override any method like so, just note that it's applied globally
+		      
+		    }
+		  }
+	});
+
 
 	// $('.option').click(function(event) {
 	// 	$('#cliches').fadeOut(500);
