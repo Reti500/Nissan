1de4075c1fba9ba51419fdaab52349691d8893fb
solorulo <solorulo@hotmail.com>
2014-08-04 21:00:25 -0500

    Se agregan algunas imagenes al index, la seccion de preguntas esta independiente

---
+++ LEAF/js/index.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	$("#mySpriteSpin").spritespin({
+		// path to the source images.
+		source: [
+		"360-LEAF-EXTERIOR/leaf360ext_0000.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0002.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0004.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0006.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0008.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0010.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0012.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0014.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0016.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0018.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0020.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0022.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0024.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0026.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0028.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0030.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0032.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0034.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0036.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0038.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0040.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0042.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0044.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0046.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0048.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0050.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0052.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0054.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0056.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0058.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0060.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0062.jpg",
+		"360-LEAF-EXTERIOR/leaf360ext_0064.jpg",
+		],
+		width   : 520,  // width in pixels of the window/frame
+		height  : 327,  // height in pixels of the window/frame
+		animate : false,
+	});
+		
+});
